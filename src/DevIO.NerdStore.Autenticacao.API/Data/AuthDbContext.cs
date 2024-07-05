ae8c4967ce5f3a04c5a48eb732a2d87aed4b6afe
Lucas Santi <idlucassanti@hotmail.com>
2024-07-04 23:35:38 -0300

    Configuração classe de DbContext para aplicação dar suporte EntityFramework

---
+++ src/DevIO.NerdStore.Autenticacao.API/Data/AuthDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.NerdStore.Autenticacao.API
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            :base(options)
+        {
+            
+        }
+    }
+}
