a54c21d0bca93e295d8ca53baaba0ec93ee39e1c
Lucas Santi <idlucassanti@hotmail.com>
2024-07-05 00:20:04 -0300

    Criação dos Modelos de dados para fluxo de criação e login de usuário

---
+++ src/DevIO.NerdStore.Autenticacao.API/Models/UsuarioLoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DevIO.NerdStore.Autenticacao.API.Models
+{
+    public class UsuarioLoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
---
+++ src/DevIO.NerdStore.Autenticacao.API/Models/UsuarioRegistroViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DevIO.NerdStore.Autenticacao.API.Models
+{
+    public class UsuarioRegistroViewModel
+    {
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public string SenhaConfirmacao { get; set; }
+    }
+}
